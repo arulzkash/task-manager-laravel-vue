bf75401905c187f2ef7fd5ed33c77e85c180943b
arulzkash <arulzkash@gmail.com>
2026-01-16 11:57:51 +0700

    chore(branding): update favicon to match Level Life logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Level Life') }}</title>
 
         <!-- Fonts -->
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
@@ -15,7 +17,9 @@
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
+
     <body class="font-sans antialiased bg-slate-900 text-gray-100">
         @inertia
     </body>
+
 </html>
